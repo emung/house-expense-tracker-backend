f92e9166119a56b717658544bfe91ec399d3d5b7
emung <emanuel.ungheanu@hotmail.com>
2025-06-04 10:25:32 +0200

    feat: add initial expense entries to queries.sql

---
+++ queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO expenses(amount, description, category) VALUES (123.45, 'Test1', 'Cat1');
+INSERT INTO expenses(amount, description, category) VALUES (67.89, 'Test2', 'Cat1');
+INSERT INTO expenses(amount, description, category) VALUES (12.34, 'Test3', 'Cat1');
+INSERT INTO expenses(amount, description, category) VALUES (45.12, 'Test4', 'Cat2');
+INSERT INTO expenses(amount, description, category) VALUES (98.99, 'Test5', 'Cat2');
+INSERT INTO expenses(amount, description, category) VALUES (44.09, 'Test6', 'Cat2');
