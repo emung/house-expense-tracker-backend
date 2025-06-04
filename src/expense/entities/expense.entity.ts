44578cadb6830e198f623a6cd02c0da1d8707d41
Emanuel <emanuel.ungheanu@prolion.com>
2025-06-04 12:37:42 +0200

    refactor: update project name and description in package.json
    style: standardize formatting in expense.entity.ts and update column types

--- src/expense/entities/expense.entity.ts
+++ src/expense/entities/expense.entity.ts
@@ -1,19 +1,19 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('expenses')
 export class Expense {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 2})
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
   amount: number;
 
-  @Column({name: 'date_added'})
+  @Column({ name: 'date_added' })
   date: Date;
 
-  @Column({type: 'string', length: 255})
+  @Column({ type: 'varchar', length: 255 })
   description: string;
 
-  @Column({type: 'string', length: 255})
+  @Column({ type: 'varchar', length: 255 })
   category: string;
 }
