a98441474abe151c3cac224307c0fef56654265f
emung <emanuel.ungheanu@hotmail.com>
2025-06-04 10:25:48 +0200

    feat: create migration for expenses table

---
+++ migrations/1748958076000-CreateExpenses.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateExpenses1748958076000 implements MigrationInterface {
+  name: string = 'CreateExpenses1748958076000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE "expenses"(
+        id SERIAL PRIMARY KEY,
+        amount NUMERIC NOT NULL,
+        date_added TIMESTAMP NOT NULL DEFAULT now(),
+        description VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL)`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "expenses";`);
+    
+  }
+
+
+}
