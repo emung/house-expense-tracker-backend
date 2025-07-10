97ff2d7fb9f93d08ee65d80c5cac6195e2e661bc
emung <emanuel.ungheanu@hotmail.com>
2025-07-10 16:51:19 +0200

    feat: add currency field to expenses table

--- migrations/1748958076000-CreateExpenses.ts
+++ migrations/1748958076000-CreateExpenses.ts
@@ -1,22 +1,20 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateExpenses1748958076000 implements MigrationInterface {
-  name: string = 'CreateExpenses1748958076000';
+    name: string = 'CreateExpenses1748958076000';
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
       CREATE TABLE "expenses"(
         id SERIAL PRIMARY KEY,
         amount NUMERIC NOT NULL,
         date_added TIMESTAMP NOT NULL DEFAULT now(),
         description VARCHAR(255) NOT NULL,
-        category VARCHAR(255) NOT NULL)`);
-  }
+        category VARCHAR(255) NOT NULL,
+        currency VARCHAR(255) NOT NULL)`);
+    }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "expenses";`);
-    
-  }
-
-
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "expenses";`);
+    }
 }
