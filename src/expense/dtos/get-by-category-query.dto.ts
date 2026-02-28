d1bd8a33d8a52817ec6dc6f7e6608a5e2c63157a
emung <emanuel.ungheanu@hotmail.com>
2026-02-28 18:50:30 +0100

    refactor: add ApiProperty decorator for query params

--- src/expense/dtos/get-by-category-query.dto.ts
+++ src/expense/dtos/get-by-category-query.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GetByCategoryQueryDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   category: string;
